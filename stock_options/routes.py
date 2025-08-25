9b5a6125d37e19be199e95d4618cd091120d7f08
Harnish Shah <hrsh6845@gmail.com>
2025-08-25 17:44:34 -0400

    fixed a failing tets.

--- stock_options/routes.py
+++ stock_options/routes.py
@@ -11,3 +11,6 @@ def get_monday_options():
     symbol = request.args.get('symbol', 'AAPL')
     calculator = MondayCalculator(symbol)
     return jsonify(calculator.get_monday_options())
+
+#@bp.route('/my-test-history', methods=['GET'])
+#def get_my_test_history():
